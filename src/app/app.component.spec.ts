4adacc3af60e37ba55db2c6f1c824b9182e5dc48
itzel <itzelreyes1299@gmail.com>
2020-11-19 23:30:12 -0600

    Pipesbuscador

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Practica3'`, () => {
+  it(`should have as title 'Busqueda'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Practica3');
